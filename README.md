c24d8ae40b179cc491548c75c82dbeb5b2bea686
Sergey Morozov <sergey.morozov@emc.com>
2017-05-04 16:53:39 +0300

    model without fit_generator

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # behavioral-cloning
 
 Augmented Data: https://yadi.sk/d/A9yVNmkT3HfUnF
+
 Raw       Data: https://yadi.sk/d/YbsZ2m-H3HfUte
--- model.py
+++ model.py
@@ -1,23 +1,64 @@
 import os
 import zipfile
+import cv2
+import numpy as np
+import pandas as pd
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Convolution2D, Cropping2D
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 # extract augmented data archive if not already extracted
 if not (os.path.isdir("data_augmented") and os.path.exists("data_augmented.csv")):
-    zip_ref = zipfile.ZipFile("data_raw.augmented", 'r')
+    zip_ref = zipfile.ZipFile("data_augmented.zip", 'r')
     zip_ref.extractall(".")
     zip_ref.close()
+
+## define generator which will help to overcome problems arising while working with a large amounts of data
+#
+#train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+#
+#def generator(samples, batch_size=128):
+#    num_samples = len(samples)
+#    while 1: # Loop forever so the generator never terminates
+#        shuffle(samples)
+#        for offset in range(0, num_samples, batch_size):
+#            batch_samples = samples[offset:offset+batch_size]
+#
+#            images = []
+#            angles = []
+#            for batch_sample in batch_samples:
+#                name = './IMG/'+batch_sample[0].split('/')[-1]
+#                center_image = cv2.imread(name)
+#                center_angle = float(batch_sample[3])
+#                images.append(center_image)
+#                angles.append(center_angle)
+#
+#            # trim image to only see section with road
+#            X_train = np.array(images)
+#            y_train = np.array(angles)
+#            yield shuffle(X_train, y_train)
 
 
 # construct neural network model
 # using the model from
 #                http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
 
-# model parameters
-steering_angle_correction = 0.2
+## model parameters
+#steering_angle_correction = 0.2
+
+data = pd.read_csv("data_augmented.csv").to_dict(orient='list')
+
+n_records = len(data['STEERING_ANGLE'])
+
+X_train = list()
+y_train = list()
+
+for i in range(n_records):
+    X_train.append(cv2.imread(data['CENTER_IMAGE'][i]))
+    y_train.append(data['STEERING_ANGLE'][i])
 
 # the model's type is sequential
 model = Sequential()
@@ -29,16 +70,20 @@ model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
 # normalize input image
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
+# add 5 convolution layers
 model.add(Convolution2D(filters=24, kernel_size=(5, 5)))
 model.add(Convolution2D(filters=36, kernel_size=(5, 5)))
 model.add(Convolution2D(filters=48, kernel_size=(5, 5)))
 model.add(Convolution2D(filters=64, kernel_size=(3, 3)))
 model.add(Convolution2D(filters=64, kernel_size=(3, 3)))
 
+# flatten output of the last convolution layer
 model.add(Flatten())
 
-# model has only one output - steering angle
-model.add(Dense(1))
+# add some fully connected layers
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(1))  # model has only one output - steering angle
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, verbose=1)
