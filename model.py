62ab6a5a568ef0a1a493f852d15451495e0f1d52
Sergey Morozov <sergey.morozov@emc.com>
2017-05-04 18:28:02 +0300

    using fit_generator

--- model.py
+++ model.py
@@ -10,6 +10,27 @@ from keras.layers.convolutional import Convolution2D, Cropping2D
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
+
+def generator(samples, batch_size=128):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                center_image = cv2.imread(data['CENTER_IMAGE'][batch_sample])
+                center_angle = float(data['STEERING_ANGLE'][batch_sample])
+                images.append(center_image)
+                angles.append(center_angle)
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+
+            yield shuffle(X_train, y_train)
+
+
 # extract augmented data archive if not already extracted
 if not (os.path.isdir("data_augmented") and os.path.exists("data_augmented.csv")):
     print("unzipping data")
@@ -17,51 +38,21 @@ if not (os.path.isdir("data_augmented") and os.path.exists("data_augmented.csv")
     zip_ref.extractall(".")
     zip_ref.close()
 
-## define generator which will help to overcome problems arising while working with a large amounts of data
-#
-#train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-#
-#def generator(samples, batch_size=128):
-#    num_samples = len(samples)
-#    while 1: # Loop forever so the generator never terminates
-#        shuffle(samples)
-#        for offset in range(0, num_samples, batch_size):
-#            batch_samples = samples[offset:offset+batch_size]
-#
-#            images = []
-#            angles = []
-#            for batch_sample in batch_samples:
-#                name = './IMG/'+batch_sample[0].split('/')[-1]
-#                center_image = cv2.imread(name)
-#                center_angle = float(batch_sample[3])
-#                images.append(center_image)
-#                angles.append(center_angle)
-#
-#            # trim image to only see section with road
-#            X_train = np.array(images)
-#            y_train = np.array(angles)
-#            yield shuffle(X_train, y_train)
+# define generator which will help to overcome problems arising while working with a large amounts of data
+data = pd.read_csv("data_augmented.csv").to_dict(orient='list')
 
+n_records = len(data['STEERING_ANGLE'])
+
+train_samples, validation_samples = train_test_split(np.array([i for i in range(n_records)]), test_size=0.2)
+train_generator = generator(train_samples, batch_size=128)
+validation_generator = generator(validation_samples, batch_size=128)
 
 # construct neural network model
 # using the model from
 #                http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
 
-## model parameters
-#steering_angle_correction = 0.2
-
-print('reading data')
-
-data = pd.read_csv("data_augmented.csv").to_dict(orient='list')
-
-n_records = len(data['STEERING_ANGLE'])
-
-X_train = list()
-y_train = list()
-
-for i in range(n_records):
-    X_train.append(cv2.imread(data['CENTER_IMAGE'][i]))
-    y_train.append(data['STEERING_ANGLE'][i])
+# model parameters
+steering_angle_correction = 0.2
 
 # the model's type is sequential
 model = Sequential()
@@ -91,7 +82,13 @@ model.add(Dense(1))  # model has only one output - steering angle
 print('starting training')
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(np.array(X_train), np.array(y_train), validation_split=0.2, shuffle=True, verbose=1, nb_epoch=30)
+
+model.fit_generator(train_generator,
+                    samples_per_epoch=len(train_samples),
+                    validation_data=validation_generator,
+                    nb_val_samples=len(validation_samples),
+                    nb_epoch=30,
+                    verbose=1)
 
 # save model to be able to reuse it in autonomous driving
 model.save("model.h5")
