e8d8ac2d51621f07253f763da2058f6b466be3a7
Sergey Morozov <sergey.morozov@emc.com>
2017-05-05 03:29:15 +0300

    model impoved

--- model.py
+++ model.py
@@ -3,8 +3,9 @@ import zipfile
 import cv2
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from sklearn.model_selection import train_test_split
@@ -24,6 +25,20 @@ def generator(samples, batch_size=128):
                 center_angle = float(data['STEERING_ANGLE'][batch_sample])
                 images.append(center_image)
                 angles.append(center_angle)
+                
+                left_image = cv2.imread(data['LEFT_IMAGE'][batch_sample])
+                left_angle = float(data['STEERING_ANGLE'][batch_sample]) + 0.25
+                if left_angle > 1.0:
+                    left_angle = 1.0
+                images.append(left_image)
+                angles.append(left_angle)
+                
+                right_image = cv2.imread(data['RIGHT_IMAGE'][batch_sample])
+                right_angle = float(data['STEERING_ANGLE'][batch_sample]) - 0.25
+                if right_angle < -1.0:
+                    right_angle = -1.0
+                images.append(right_image)
+                angles.append(right_angle)
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
@@ -44,15 +59,15 @@ data = pd.read_csv("data_augmented.csv").to_dict(orient='list')
 n_records = len(data['STEERING_ANGLE'])
 
 train_samples, validation_samples = train_test_split(np.array([i for i in range(n_records)]), test_size=0.2)
-train_generator = generator(train_samples, batch_size=128)
-validation_generator = generator(validation_samples, batch_size=128)
+train_generator = generator(train_samples, batch_size=256)
+validation_generator = generator(validation_samples, batch_size=256)
 
 # construct neural network model
 # using the model from
 #                http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf
 
 # model parameters
-steering_angle_correction = 0.2
+steering_angle_correction = 0.25
 
 # the model's type is sequential
 model = Sequential()
@@ -82,15 +97,31 @@ model.add(Dense(1))  # model has only one output - steering angle
 
 print('starting training')
 
-model.compile(loss='mse', optimizer='adam')
+if os.path.exists("model.h5"):
+    model = load_model("model.h5")
+    print("existing model loaded")
+else:
+    model.compile(loss='mse', optimizer='adam')
 
-model.fit_generator(train_generator,
-                    samples_per_epoch=len(train_samples),
+history_object = model.fit_generator(train_generator,
+                    samples_per_epoch=3*len(train_samples),
                     validation_data=validation_generator,
-                    nb_val_samples=len(validation_samples),
-                    nb_epoch=30,
+                    nb_val_samples=3*len(validation_samples),
+                    nb_epoch=8,
                     verbose=1)
+
 
 # save model to be able to reuse it in autonomous driving
 model.save("model.h5")
 
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
